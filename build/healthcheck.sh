ac65f8a8c42486be120e7d73ccdd3cdca70c3cb6
SATO Daisuke <densuke@fuga.jp>
2025-06-03 06:15:31 +0900

    -hの重複(実用性の都合で一旦db向きに)

--- build/healthcheck.sh
+++ build/healthcheck.sh
@@ -2,6 +2,6 @@
 
 
 # Wait for the database to be ready
-mysqladmin ping -h localhost --silent -h db \
+mysqladmin ping --silent -h db \
     -u "${MYSQL_USER}" --password="${MYSQL_PASSWORD}" \
     --wait=1
